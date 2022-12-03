db25962f39a38a073f7f16bfc7513a7b006553ab
vitaliybarahovskiy <vitalikbarahovskii@mail.ru>
2022-12-03 23:01:17 +0300

    Upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,21 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route,Routes} from "react-router-dom";
+import {Route,Routes} from "react-router-dom";
 
 function App (props: any) {
 
     return (
-        <BrowserRouter>
             <div className='app-wrapper'>
               <Header />
               <Navbar />
               <div className='app-wrapper-content'>
               <Routes>
-                <Route path="/dialogs" element={<Dialogs state={props.state.dialogsPage} messages={props.state.dialogsPage.messages}/>} />
+                <Route path="/dialogs" element={<Dialogs state={props.state.dialogsPage} />} />
                 <Route path="/profile" element={<Profile state={props.state.profilePage} />} />
               </Routes>
               </div>
             </div>
-        </BrowserRouter>
   );
 }
 
