328d54b24839548901b31826c55f3c75d3fd41ef
37525 <vitalikbarahovskii@mail.ru>
2023-01-31 17:29:31 +0300

    Great Users extend component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,10 +6,19 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return <nav className={classes.nav}>
         <div className={classes.item}>
-            <NavLink className={(navData) => navData.isActive ? "active" : classes.activeLink } to="/profile">Profile</NavLink>
+            <NavLink className={(navData) =>
+                navData.isActive ? "active" : classes.activeLink }
+                     to="/profile">Profile</NavLink>
         </div>
         <div className={classes.item}>
-            <NavLink className={(navData) => navData.isActive ? "active" : classes.activeLink } to="/dialogs">Messages</NavLink>
+            <NavLink className={(navData) =>
+                navData.isActive ? "active" : classes.activeLink }
+                     to="/dialogs">Messages</NavLink>
+        </div>
+        <div className={classes.item}>
+            <NavLink className={(navData) =>
+                navData.isActive ? "active" : classes.activeLink }
+                     to="/users">Users</NavLink>
         </div>
         <div className={classes.item}>
             <NavLink to="/news">News</NavLink>
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,39 +2,43 @@ import React from "react";
 import styles from './users.module.css'
 import {UsersPropsType} from "./UsersContainer";
 import axios from "axios";
-// import * as axios from "axios";
 
 
-let Users = (props: UsersPropsType) => {
-    if (props.usersPage.users.length === 0) {
 
+
+class Users extends React.Component<UsersPropsType>{
+
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            // console.log(response.data.items)
-            props.setUsers(response.data.items)
+            this.props.setUsers(response.data.items)
         })
     }
-    console.log(props.usersPage.users)
-    return <div>
-        {
-            props.usersPage.users.map(u => {
-                return <div key={u.id}>
+
+    render() {
+        return <div>
+            {
+                this.props.usersPage.users.map(u => {
+                    return <div key={u.id}>
                     <span>
                     <div>
-                        <img src={u.photos.small ? u.photos.small : ''} className={styles.userPhoto}/>
+                        <img src={u.photos.small ? u.photos.small :
+                            'https://kartinkof.club/uploads/posts/2022-03/1648291448_1-kartinkof-club-p-lisii-muzhik-mem-1.jpg'}
+                             className={styles.userPhoto}/>
                     </div>
                     <div>
 
                         {u.followed ?
                             <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button> :
                             <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
 
                     </div>
                 </span>
-                <span>
+                        <span>
                         <div>
                             {u.name}
                         </div>
@@ -42,10 +46,11 @@ let Users = (props: UsersPropsType) => {
                             {u.status}
                         </div>
                 </span>
-                </div>
-            })
-        }
-    </div>
+                    </div>
+                })
+            }
+        </div>
+    }
 }
 
-export default Users
+export default Users;
