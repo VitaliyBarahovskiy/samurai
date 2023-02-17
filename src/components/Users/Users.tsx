2660e79b2c0ceb7e34d156fab51bf85730e0567e
37525 <vitalikbarahovskii@mail.ru>
2023-02-17 18:45:31 +0300

    Add follow and unfollow

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./users.module.css";
 import {UsersType} from "../../redux/users-reduce";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 type UsersNewPropsType = {
     totalUsersCount: number
@@ -47,10 +48,31 @@ let Users = (props: UsersNewPropsType) => {
 
                         {u.followed ?
                             <button onClick={() => {
-                                props.unfollow(u.id)
+                                axios.delete( `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "8fafdb26-5c99-4181-bb7c-189458423534"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode === 0){
+                                            props.unfollow(u.id)
+                                        }
+                                    })
+
                             }}>Unfollow</button> :
                             <button onClick={() => {
-                                props.follow(u.id)
+                                axios.post( `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "8fafdb26-5c99-4181-bb7c-189458423534"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode === 0){
+                                            props.follow(u.id)
+                                        }
+                                    })
                             }}>Follow</button>}
 
                     </div>
