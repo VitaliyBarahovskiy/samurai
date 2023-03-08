ba4252584812d5e6510f46fac601641395b6acd7
37525 <vitalikbarahovskii@mail.ru>
2023-03-08 16:33:03 +0300

    Redux-form Part1

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 
-
-const LoginPage = (props: any) => {
-    return <h1>LOGIN</h1>
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
 }
 
 
-export default LoginPage
+const Login = () => {
+
+    const onSubmit = (formData: FormDataType)=> {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    );
+};
+
+export default Login;
+
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return (
+        <form action="" onSubmit={props.handleSubmit}>
+            <Field component={'input'} name={'login'} placeholder={'login'}/>
+            <Field component={'input'} name={'password'} placeholder={'password'}/>
+            <Field component={'input'} name={'rememberMe'} type="checkbox" /> remember Me
+            <button>Login</button>
+
+        </form>
+
+    );
+};
+
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -4,14 +4,16 @@ import profileReduce from "./profile-reduce";
 import dialogsReduce from "./dialogs-reducer";
 import usersReduce from "./users-reduce";
 import {authReducer} from "./auth-reduce";
-import thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 
 
 export const rootReduce = combineReducers({
     profilePage: profileReduce,
     dialogsPage: dialogsReduce,
     usersPage: usersReduce,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 
